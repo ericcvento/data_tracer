ff960ac1b268b999c348fe3a49bdf4403dabb5f3
Eric Christopher Vento <vento.eric@gmail.com>
2022-05-09 13:10:19 -0400

    added functions to sort sas and stata programs from filelist

--- data_tracer.py
+++ data_tracer.py
@@ -1,17 +1,34 @@
 from os import walk
 
-def MakeFileList(path):
-    print("Scanning Directory...")
+def make_file_list(path):
     filelist=[]
     for dir, subdirs, fnames in walk(path):
         for fname in fnames:
             fullpath=str('{}\{}'.format(dir,fname))
             filelist.append(fullpath)
-    print(str('{} files found!'.format(len(filelist))))
     return filelist
 
+def get_sas_programs(filelist):
+    sasprograms=[]
+    for f in filelist:
+        if ".SAS" in f.upper(): 
+            sasprograms.append(f)
+    return sasprograms  
+
+def get_stata_programs(filelist):
+    stataprograms=[]
+    for f in filelist:
+        if ".DO" in f.upper(): 
+            stataprograms.append(f)
+    return stataprograms  
+
 def data_tracer(): 
-    print(MakeFileList("TESTprojectfolder")) 
+    filelist=make_file_list("TESTprojectfolder")
+    sasprograms=get_sas_programs(filelist)
+    stataprograms=get_stata_programs(filelist)
 
+    print(sasprograms)
+    print(stataprograms)
 
+#call main
 data_tracer() 
